bbaa4be6edc5ef32d0c3cf85cf9384b9e7c9afed
Jashwanth Alwaru <112063260+Jashwnath@users.noreply.github.com>
2025-07-25 22:39:30 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,30 @@
 //your JS code here. If required.
+const changeButton=document.getElementById("change_button");
+const rstButton=document.getElementById("Reset");
+
+changeButton.addEventListener("click",function(){
+
+const blockId = document.getElementById("block_id").value.trim();
+const colour = document.getElementById("colour_id").value.trim();
+
+	const allItems = document.querySelectorAll(".grid-item");
+	allItems.forEach(item=>{
+		item.style.backgroundColor = "transparent";
+	})
+	const targetItem=document.getElementById(blockId);
+	if(targetItem){
+		targetItem.style.backgroundColor=colour;
+	}
+	else{
+		alert("enter a valid colour");
+	}
+})
+rstButton.addEventListener("click",function(){
+const allItems = document.querySelectorAll(".grid-item");
+	allItems.forEach(item=>{
+		item.style.backgroundColor="transparent";
+	})
+})
+
+document.getElementById("block_id").value="";
+document.getElementById("colour_id").value="";
